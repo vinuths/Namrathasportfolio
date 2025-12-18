c34d74586b57367f64fc2b1a5d749b713ab26b73
Vinuth S <vinuthgowda85@gmail.com>
2025-12-18 16:47:41 +0530

    deploy to netlify

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -3,15 +3,33 @@ import Typewriter from "typewriter-effect";
 
 function Type() {
   return (
-    <div style={{ textAlign: "center", fontSize: "1.5em" }}>
+    <div className="typewriter-container">
       <Typewriter
         options={{
           strings: ["Technical Analyst"],
           autoStart: true,
           loop: true,
           deleteSpeed: 50,
+          delay: 75,
         }}
       />
+
+      {/* Responsive styles */}
+      <style jsx>{`
+        .typewriter-container {
+          width: 100%;
+          text-align: center;
+          overflow: hidden;
+        }
+
+        .typewriter-container span {
+          display: inline-block;
+          white-space: normal;
+          word-break: break-word;
+          font-size: clamp(1.1rem, 4vw, 1.6rem);
+          line-height: 1.4;
+        }
+      `}</style>
     </div>
   );
 }
